f6d58fd0aec3caa554361bc70c4d7b967f943f31
Dave Gillie <davecgillie@gmail.com>
2016-02-09 23:35:53 -0600

    I believe these are good test cases for post request create functionality. Implement the server code now.

--- keyword-wrangler/features/post-request.feature
+++ keyword-wrangler/features/post-request.feature
@@ -4,6 +4,6 @@ Feature: Creating new database entries
 	So that the app actually does something
 
 	Scenario: The web server api should respond to post requests
-		Given a running server
+		Given a running server with 'preload' values in database
 		When I make a post request to /api/keywords/
 		Then the server should create a new database entry
--- keyword-wrangler/features/step-definitions/post-request.js
+++ keyword-wrangler/features/step-definitions/post-request.js
@@ -8,16 +8,93 @@ var mockAbstract = require('../../db-abstract/mockAbstract');
 
 module.exports = function () {
 	
-	this.Given(/^a running server$/, function (callback) {
-	  callback.pending();
+	const PORT = 8081;
+	const URL = 'http://localhost:'+PORT+'/api/keywords/';
+	var server;
+	var lu = {
+		'_id': 9000,
+		'name': 'Dr. Lu',
+		'category': 'male'
+	};
+	var preload = [
+      {'_id': 1, 'name': 'Dave Gillie', 'category': 'male'},
+      {'_id': 2, 'name': 'Kaitlyn Gillie', 'category': 'female'}
+    ];
+    var expected = preload.slice();
+    expected.push(lu);
+
+	this.Given(/^a running server with 'preload' values in database$/, function (callback) {
+		server = Server(PORT);
+		async.series([
+			function (callback) {
+				mockAbstract.delete(function (deleted) {
+					callback();
+				});
+			},
+			function (callback) {
+				mockAbstract.create(preload, function (count) {
+					callback();
+				});
+			},
+			function (callback) {
+				server.listen(function (err) {
+					if (err) {
+						console.error(err);
+					}
+					callback();
+				});
+			}
+		], function (err, results) {
+			callback();
+		});
 	});
 
 	this.When(/^I make a post request to \/api\/keywords\/$/, function (callback) {
-	  callback.pending();
+		 request.post(
+		  	{
+		  		'url': URL,
+		  		'body': lu,
+		  		'json': true
+		  	},
+		  	function (err, res, body) {
+		  		assert.equal(res.statusCode, 200);
+		  		callback();
+		  	});
 	});
 
 	this.Then(/^the server should create a new database entry$/, function (callback) {
-	  callback.pending();
+		var actual = {};
+		async.series(
+			[
+				function (callback) {
+					request.get(
+					{
+						'url': URL,
+						'json': true
+					},
+					function (err, res, body) {
+						actual.statusCode = res.statusCode;
+						actual._items = body._items;
+						callback();
+					});
+				},
+				function (callback) {
+					mockAbstract.delete(function (deleted) {
+	  					callback();
+					})
+				},
+				function (callback) {
+					server.close(function () {
+						callback();
+					});
+				}
+			],
+			function (err, results) {
+				assert.equal(actual.statusCode, 200);
+	  			assert.deepEqual(actual._items, expected);
+	  			callback();
+			}
+		);
 	});
 
 }//module.exports = function ()
