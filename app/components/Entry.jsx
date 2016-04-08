d8607028af60a9b4077ecec3083a1da94ac55d0f
Spencer Smith <spencer.smith.t@gmail.com>
2016-04-08 18:14:59 -0500

    Some addMeaning Functionality

--- app/components/Entry.jsx
+++ app/components/Entry.jsx
@@ -1,16 +1,40 @@
 var React = require('react');
 
-var AddMeaningForm = require('./AddMeaningForm.jsx');
+var MeaningsContainer = require('./MeaningsContainer.jsx');
 
 module.exports = React.createClass({
+
+  addMeaning: function(def, parPath) {
+    var self = this;
+    var doc = this.props.doc;
+    //console.log((doc.data[parPath].meanings).length);
+    var arrayPosition = (doc.data[parPath].meanings).length;
+    console.log("arr:", arrayPosition);
+    var newPath = parPath.slice();
+
+    //This should dynamically generate the path.
+    newPath.push("meanings");
+    newPath.push(arrayPosition);
+
+    var ins_obj = {meaning: def, path: newPath};
+    doc.submitOp([{p: newPath, li: ins_obj}], function() {
+      self.setState({
+        entries: doc.data
+      });
+    });
+  },
+
   render: function() {
     var entry = this.props.entry;
-    var output = JSON.stringify(entry);
-    return (
-      <div>
-        <div>{output}</div>
-        <AddMeaningForm parentPath={entry ? entry.path : []} />
-      </div>
-    );
+    if (entry) {
+      return (
+        <div>
+          <div>{entry.word}</div>
+          <MeaningsContainer addMeaning={this.addMeaning} entry={this.props.entry} parentPath={this.props.parentPath}/>
+        </div>
+      );
+    } else {
+        return(<div>No Word Yet!</div>);
+    }
   }
 });
