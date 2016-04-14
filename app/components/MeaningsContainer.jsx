4c6d137feabb48c0a97c71d7dbf3d6acb5a13b16
Dave Gillie <davecgillie@gmail.com>
2016-04-14 00:21:21 -0500

    update meanings array by deleting and inserting entire meaning field

--- app/components/MeaningsContainer.jsx
+++ app/components/MeaningsContainer.jsx
@@ -4,16 +4,36 @@ module.exports = React.createClass({
   removeMeaning: function(current) {
     var self = this;
     var doc = self.props.doc;
-    var copy = JSON.parse(JSON.stringify(current));
     return (function() {
-      var entryKey = copy.path[0];
+      var entryKey = current.path[0];
       var meaningsLength = doc.data[entryKey].meanings.length;
-      var indexOfDeleted = copy.path[2];
+      var indexOfDeleted = current.path[2];
       var ops = [];
       for(var i = indexOfDeleted + 1; i < meaningsLength; i++) {
         ops.push({p: [entryKey, "meanings", i, "path", 2], na: -1});
       }
-      ops.push({p: copy.path, ld: copy});
+      ops.push({p: current.path, ld: current});
+      doc.submitOp(ops, function() {
+        self.setState({
+          entry: doc.data[entryKey]
+        });
+      });
+    });
+  },
+  updateMeaning: function(meaning) {
+    var self = this;
+    var doc = self.props.doc;
+    return (function() {
+      var entryKey = meaning.path[0];
+      var inputKey = entryKey + meaning.path[2];
+      var input = document.getElementById(inputKey);
+      var copyOfMeaningPath = JSON.parse(JSON.stringify(meaning.path));
+      copyOfMeaningPath.push("meaning");
+      copyOfMeaningPath.push(0);
+      var ops = [];
+      ops.push({p: copyOfMeaningPath, sd: meaning.meaning});
+      ops.push({p: copyOfMeaningPath, si: input.value});
+
       doc.submitOp(ops, function() {
         self.setState({
           entry: doc.data[entryKey]
@@ -28,9 +48,11 @@ module.exports = React.createClass({
         <div>
           {
           this.props.meanings.map(function(current, index) {
+            var key = current.path[0] + index;
             return (
               <div key={"meaning"+index}>
-                <input value={current.meaning} placeholder="Enter Meaning"></input> <button onClick={self.removeMeaning(current)}>delete</button>
+                <input id={key} value={current.meaning} placeholder="Enter Meaning" onChange={self.updateMeaning(current)}></input>
+                <button onClick={self.removeMeaning(current)}>delete</button>
               </div>
             );
           })
