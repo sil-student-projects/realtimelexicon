6ac726fd170f37c92e53e9085d32c63dd105741d
xixiz <s670937@sbuniv.edu>
2016-04-18 19:57:35 -0500

    id changed to ten random letters followed by a time stamp, so now it is able to click the 'Add Entry' button and add empty words.

--- app/functions.js
+++ app/functions.js
@@ -3,9 +3,18 @@ var exports = module.exports = {};
 exports.makeKey = function(string) {
 	if (typeof(string) == "string") {
 		var id = "";
-		for (var i = 0, len = string.length; i < len; i++) {
-			id += string.charCodeAt(i);
+		if(string == "addEntryInput") {
+			for (var i = 0, len = string.length; i < len; i++) {
+				id += string.charCodeAt(i);
+			}
 		}
+		else {
+			var randomLetter = "";
+			for(var i=0; i<10; i++) {
+				randomLetter += String.fromCharCode(65+Math.floor(Math.random()*26));
+			}
+			id = randomLetter + Date.now();
+		} //end else
 		return id;
 	} else {
 		console.log("input was not a string");
